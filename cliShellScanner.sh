a74a1a2a7864f4ee746d5c53781866528e2d253a
abemin <muhaimifatihi@hotmail.com>
2020-11-26 07:03:35 +0800

    more refined search

--- cliShellScanner.sh
+++ cliShellScanner.sh
@@ -34,7 +34,7 @@ sleep 1
 #
 
 echo "[+] Finding unique string inside file extensions..."
-for shellfile in $(cat ./recon/qualifier.txt);do grep -rnl -E 'IndoXploit'\|'b374k'\|'c99'\|'R57'\|'shell'\|'Shell'\|'Sh3ll'\|'bindport'\|'exploit'\|'sha1'\|'chunk_split'\|'back_connect'\|'backdoor'\|'file_uploads'\|'netcat'\|'/etc/passwd'\|'dbname'\|'login_shell'\|'hacked'\|'Hacked'\|'0x1999'\|'multipart/form-data'\|'base64_encode'\|'base64_decode'\|'eval'\|'function'\|'Obfuscation'\|'proxy'\|'getenv'\|'exec'\|'public_html'\|'getcwd'\|'md5_pass'\|'download'\|'Brute'\|'Backdoor'\|'Encoder'\|'urldecode'\|'defaced'\|'deface'\|'Defaced'\|'Legion'\|'Touched By'\|'WORM'\|'INTRUDER' $shellfile >> ./recon/semifinal1.txt; done
+for shellfile in $(cat ./recon/qualifier.txt);do grep -rnl -E 'IndoXploit'\|'b374k'\|'c99'\|'R57'\|'shell'\|'Shell'\|'Sh3ll'\|'bindport'\|'exploit'\|'sha1'\|'chunk_split'\|'back_connect'\|'backdoor'\|'file_uploads'\|'netcat'\|'/etc/passwd'\|'dbname'\|'login_shell'\|'hacked'\|'Hacked'\|'0x1999'\|'multipart/form-data'\|'base64_encode'\|'base64_decode'\|'eval'\|'function'\|'Obfuscation'\|'proxy'\|'getenv'\|'exec'\|'public_html'\|'getcwd'\|'md5_pass'\|'download'\|'Brute'\|'Backdoor'\|'Encoder'\|'urldecode'\|'defaced'\|'deface'\|'Defaced'\|'Legion'\|'Touched By'\|'WORM'\|'INTRUDER'\|'setcookie' $shellfile >> ./recon/semifinal1.txt; done
 sleep 1
 
 ######################################################
@@ -46,7 +46,7 @@ find $directory -type f -name '*.php' -mtime -30 >> ./recon/quaterfinal.txt
 sleep 1
 
 echo "[+] Finding unique string inside PHP file extensions..."
-for phpfile in $(cat ./recon/quaterfinal.txt);do grep -rnl -E 'IndoXploit'\|'b374k'\|'c99'\|'R57'\|'shell'\|'Shell'\|'Sh3ll'\|'bindport'\|'exploit'\|'sha1'\|'chunk_split'\|'back_connect'\|'backdoor'\|'file_uploads'\|'netcat'\|'/etc/passwd'\|'dbname'\|'login_shell'\|'hacked'\|'Hacked'\|'0x1999'\|'multipart/form-data'\|'base64_encode'\|'base64_decode'\|'eval'\|'function'\|'Obfuscation'\|'proxy'\|'getenv'\|'exec'\|'public_html'\|'getcwd'\|'md5_pass'\|'download'\|'Brute'\|'Backdoor'\|'Encoder'\|'urldecode'\|'defaced'\|'deface'\|'Defaced'\|'Legion'\|'Touched By'\|'WORM'\|'INTRUDER' $phpfile >> ./recon/semifinal.txt; done
+for phpfile in $(cat ./recon/quaterfinal.txt);do grep -rnl -E 'IndoXploit'\|'b374k'\|'c99'\|'R57'\|'shell'\|'Shell'\|'Sh3ll'\|'bindport'\|'exploit'\|'sha1'\|'chunk_split'\|'back_connect'\|'backdoor'\|'file_uploads'\|'netcat'\|'/etc/passwd'\|'dbname'\|'login_shell'\|'hacked'\|'Hacked'\|'0x1999'\|'multipart/form-data'\|'base64_encode'\|'base64_decode'\|'eval'\|'function'\|'Obfuscation'\|'proxy'\|'getenv'\|'exec'\|'public_html'\|'getcwd'\|'md5_pass'\|'download'\|'Brute'\|'Backdoor'\|'Encoder'\|'urldecode'\|'defaced'\|'deface'\|'Defaced'\|'Legion'\|'Touched By'\|'WORM'\|'INTRUDER'\|'setcookie'  $phpfile >> ./recon/semifinal.txt; done
 sleep 1
 
 echo "[+] Harvesting done..."
@@ -62,6 +62,7 @@ sort ./recon/semifinal.txt | uniq >> ./recon/prefinal.txt
 sleep 1
 
 echo "[+] Checking files for false positive..."
+
 STRING="view.html.php"
 for checkfile in $(cat ./recon/prefinal.txt);do 
 	if [[ "$checkfile" != *"$STRING"* ]];then
@@ -69,7 +70,21 @@ for checkfile in $(cat ./recon/prefinal.txt);do
 	fi
 done
 
-for stringfile in $(cat ./recon/prefinal1.txt);do grep -rnl -E 'GNU'\|'BSD'\|'opensource'\|'Opensource'\|'license'\|'LICENSE'\|'MIT'\|'ISO-8859-1'\|'Redistributions'\|'framework'\|'WARRANTY'\|'Annex'\|'Cyrillic'\|'nodecounter'\|'Sodium'\|'sodium'\|'PHPMailer'\|'log-level'\|'Unicode'\|'unicode'\|'namespace'\|'composer'\|'ReCaptcha'\|'www.php.net'\|'PCRE'\|'ASCII'\|'UTF-8'\|'utf-8' $stringfile  >> ./recon/prefinal2.txt;done
+STRING="module.audio"
+for checkfile in $(cat ./recon/prefinal1.txt);do 
+	if [[ "$checkfile" == *"$STRING"* ]];then
+		echo $checkfile >> ./recon/prefinal2.txt
+	fi
+done
+
+STRING="module.tag"
+for checkfile in $(cat ./recon/prefinal1.txt);do 
+	if [[ "$checkfile" == *"$STRING"* ]];then
+		echo $checkfile >> ./recon/prefinal2.txt
+	fi
+done
+
+for stringfile in $(cat ./recon/prefinal1.txt);do grep -rnl -E 'GNU'\|'BSD'\|'opensource'\|'Opensource'\|'license'\|'LICENSE'\|'Redistributions'\|'framework'\|'WARRANTY'\|'Annex'\|'Cyrillic'\|'nodecounter'\|'Sodium'\|'sodium'\|'PHPMailer'\|'log-level'\|'Unicode'\|'unicode'\|'composer'\|'ReCaptcha'\|'www.php.net'\|'PCRE'\|'ASCII'\|'utf-8'\|'WordPress'\|'wp_scripts'\|'Etag'\|'akismet'\|'hello_dolly'\|'IDNA'\|'Polyfill'\|'logger'\|'IXR_'\|'Requests_Hooks'\|'AtomLib'\|'WP_Date_Query'\|'Translation_Entry'\|'POMO_Reader'\|'Requests_Cookie'\|'Requests_Exception'\|'Requests_Hooker'\|'Requests_Response'\|'Requests_Utility'\|'Plural_Forms' $stringfile  >> ./recon/prefinal2.txt;done
 
 echo "[+] Sorting and removing duplicates line..."
 sort ./recon/prefinal1.txt | uniq > ./recon/prefinal3.txt
